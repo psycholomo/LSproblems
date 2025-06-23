56acd77c5a33c7dd824ba8d4f84b3d0064ab7b0b
Joshua Crane <josh@bintree.io>
2025-06-23 20:10:41 +0900

    added more folders and files

---
+++ courses/py101/practice_problems/easy1.py
@@ -0,0 +1,13 @@
+#1 yes because 6 is out of bounds
+
+#2
+str1 = "Come over here!"  # True
+str2 = "What's up, Doc?"  # False
+
+def end_exclamation(input_string):
+    if  input_string[-1] == "!":
+        return True
+    
+    return False
+
+print(end_exclamation(str1))
