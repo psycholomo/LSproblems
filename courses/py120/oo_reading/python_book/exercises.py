52152808d5289ba861c25c50004e4599ca553a55
Joshua Crane <josh@bintree.io>
2025-07-17 22:15:34 +0900

    finished py 119 and 109, made more notes for 120

---
+++ courses/py120/oo_reading/python_book/exercises.py
@@ -0,0 +1,15 @@
+class Pet():
+    def __init__(self, name):
+        self.name = name
+        type_name = type(self).__name__
+        print(f'{type_name}')
+
+
+george = Pet('george')
+print(george.name)
+ralph = Pet('Raulph')
+
+
+class Foo():
+    def __init__(self):
+        print(f'{type(self).__name__}')
