3e1e930f1cf0011f14d72bdf2338da684e9ffd88
JOSH CRANE <onji@JOSHs-MacBook-Pro.local>
2025-08-18 21:07:28 -0700

    created todo app

---
+++ courses/py130/todolist/todolistapp/todolist.py
@@ -0,0 +1,109 @@
+class Todo:
+    IS_DONE = "X"
+    IS_UNDONE = " "
+
+    def __init__(self, title):
+        self._title = title
+        self._done = False
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @property
+    def done(self):
+        return self._done
+    
+    @done.setter
+    def done(self, done):
+        self._done = done
+    
+    def __str__(self):
+        marker = Todo.IS_DONE if self.done else Todo.IS_UNDONE
+        return f'[{marker}] {self.title}'
+    
+    def __eq__(self, other):
+        if isinstance(other, Todo):
+            return self.title == other.title and self.done == other.done
+        
+        return NotImplemented
+    
+
+class TodoList:
+    def __init__(self, title):
+        self._title = title
+        self._todos = []
+    
+    @property
+    def title(self):
+        return self._title
+
+
+    def add(self, todo):
+        if isinstance(todo, Todo):
+            self._todos.append(todo)
+    
+        else:
+            raise TypeError("Needs to be a todo object")
+    
+
+    def each(self, callback):
+        for todo in self._todos:
+            callback(todo)
+            
+    def __str__(self):
+        output_lines = [f'----- {self.title} -----']
+        output_lines += [str(todo) for todo in self._todos]
+        return "\n".join(output_lines)
+    
+    def __len__(self):
+        return len(self._todos)
+    
+    def first(self):
+        return self._todos[0]
+    
+    def last(self):
+        return self._todos[-1]
+    
+    def to_list(self):
+        return self._todos.copy()
+    
+    def todo_at(self, index):
+        return self._todos[index]
+    
+    def mark_done_at(self, index):
+        self.todo_at(index).done = True
+    
+    def mark_undone_at(self, index):
+        self.todo_at(index).done = False
+    
+    def mark_all_done(self):
+        for todo in self._todos:
+            todo.done = True
+    def mark_all_undone(self):
+        for todo in self._todos:
+            todo.done = False
+    
+    def all_done(self):
+        return all(todo.done for todo in self._todos)
+    
+    def remove_at(self, index):
+        self._todos.pop(index)
+
+# Code omitted for brevity.
+
+empty_todo_list = TodoList('Nothing Doing')
+
+def setup():
+    todo1 = Todo('Buy milk')
+    todo2 = Todo('Clean room')
+    todo3 = Todo('Go to gym')
+
+    todo2.done = True
+
+    todo_list = TodoList("Today's Todos")
+    todo_list.add(todo1)
+    todo_list.add(todo2)
+    todo_list.add(todo3)
+
+    return todo_list
